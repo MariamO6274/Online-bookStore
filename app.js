ad10064798fd91214f5eda4f133bfdeaa1b6ed3e
MariamO6274 <ostatishvili@gmail.com>
2023-08-18 19:26:25 -0600

    details page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
-fetch("https://example-data.draftbit.com/books?_limit=10").then((data)=>{
-    console.log(data)
+fetch("https://example-data.draftbit.com/books?_limit=10")
+.then((data)=>{
+    // console.log(data)
     return data.json()
     .then((booksObj)=> {
         console.log(booksObj);
@@ -10,9 +11,9 @@ fetch("https://example-data.draftbit.com/books?_limit=10").then((data)=>{
           allBook += `<div class="each-card">
           <img class="photoes" src=${value.image_url} alt="img" />
           <h1 class="title">${value.title}</h1>
+          <p class="author">Author: ${value.authors} </p>
           <p class="genres">Genres: ${value.genres}</p>
-          <p class="price">Price: $${value.rating}</p>
-          <button>Read more</button>
+          <a class="button" href="./Pages/details.html">Add To Cart</a>
         </div>`;
         });
         document.getElementById("cards").innerHTML = allBook;
